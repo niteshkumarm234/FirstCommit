94716dc3a8e2e61405dc9891adc4b925ec1b43ff
niteshkumar-maurya <niteshkumarm234@gmail.com>
2024-08-09 02:05:08 +0530

    First commit

---
+++ JwtToken/src/main/java/SpringSecurity/example/JwtToken/JwtTokenApplication.java
@@ -0,0 +1,13 @@
+package SpringSecurity.example.JwtToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenApplication.class, args);
+	}
+
+}
